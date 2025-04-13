de3ec9a8cc97ee393ec670f7c56f09d60c7e5fd3
a-know <a.know.dev@gmail.com>
2025-04-13 18:42:19 +0900

    edit migration file

--- app/models/deleted_user.rb
+++ app/models/deleted_user.rb
@@ -1,2 +1,3 @@
 class DeletedUser < ApplicationRecord
+  self.primary_key = :id
 end
--- db/migrate/20250413093708_create_deleted_users.rb
+++ db/migrate/20250413093708_create_deleted_users.rb
@@ -1,11 +1,13 @@
 class CreateDeletedUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :deleted_users do |t|
-      t.string :id
-      t.string :username
-      t.datetime :deleted_at
+    create_table :deleted_users, id: false do |t|
+      t.string :id, primary_key: true
+      t.string :username, null: false
+      t.datetime :deleted_at, null: false
 
       t.timestamps
     end
+
+    add_index :deleted_users, :username
   end
 end
