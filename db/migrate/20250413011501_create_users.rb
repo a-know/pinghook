c4758be4c23df5788f0e5bb5c7b0735f6b9e66cf
a-know <a.know.dev@gmail.com>
2025-04-13 10:16:53 +0900

    Modify migration

--- db/migrate/20250413011501_create_users.rb
+++ db/migrate/20250413011501_create_users.rb
@@ -1,11 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :users do |t|
-      t.string :username
-      t.string :webhook_url
-      t.string :token
+    create_table :users, id: false do |t|
+      t.string :id, primary_key: true       # UUIDを自前で使う
+      t.string :username, null: false
+      t.string :webhook_url, null: false
+      t.string :token, null: false
 
       t.timestamps
     end
+
+    add_index :users, :username, unique: true
+    add_index :users, :token, unique: true
   end
 end
