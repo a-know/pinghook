7f161809b62539384268eaeb2ef809b05c6a42a7
a-know <a.know.dev@gmail.com>
2025-04-16 00:05:45 +0900

    Make toppage

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  root to: redirect('/')
+
   namespace :api do
     namespace :v1 do
       post '/users', to: 'users#create'
